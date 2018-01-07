854d883be00f05ec17df8b6a2634fca8657e425d
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2018-01-07 07:27:47 +0530

    StackOverflow, Twitter services and components generated.

---
+++ src/app/components/stackoverflow/stackoverflow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stackoverflow',
+  templateUrl: './stackoverflow.component.html',
+  styleUrls: ['./stackoverflow.component.css']
+})
+export class StackoverflowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
