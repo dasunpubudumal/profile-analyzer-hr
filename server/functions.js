fc3ae4fc62e16400860d67a4439e26ae6afa6533
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2018-01-07 12:33:45 +0530

    Comments

--- server/functions.js
+++ server/functions.js
@@ -8,16 +8,14 @@ functions = {
     const finalheader = 'Basic ' + encheader;
 
     request.post('https://api.twitter.com/oauth2/token', { form: {'grant_type': 'client_credentials' },
-      headers: {Authorization: finalheader, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}}, function(error, response, body) {
+      headers: { Authorization: finalheader, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}}, function(error, response, body) {
       if(error) throw error;
       else {
-        console.log(body);
+        console.log(body);  //This is wht is logged in the native console.
         config.bearertoken = JSON.parse(body).access_token;
-        res.json({success: true, data: config.bearertoken});
+        res.json({success: true, data: config.bearertoken});  // This is logged in browser console.
       }
-
     });
-
   }
 };
 
