3ab842f9d2ad362846d8279be4a059b6f3bab3c2
dasunpubudumal <dasun.15@cse.mrt.ac.lk>
2018-01-07 10:03:43 +0530

    comments

--- server/functions.js
+++ server/functions.js
@@ -9,8 +9,14 @@ functions = {
     const encheader = new Buffer(header).toString('base64');
     const finalheader = 'Basic ' + encheader;
 
+    /* Check the documentation for required headers.
+     * https://developer.twitter.com/en/docs/basics/authentication/overview/application-only
+     */
+
     request.post('https://api.twitter.com/oauth2/token', { form: {'grant_type': 'client_credentials' },
-      headers: { Authorization: finalheader, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}}, function(error, response, body) {
+      headers: {
+      Authorization: finalheader,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}}, function(error, response, body) {
       if(error) throw error;
       else {
         console.log(body);  //This is wht is logged in the native console.
