0d472b5266c9c7fba260446d9597ecf7285012e2
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2018-01-07 12:30:53 +0530

    Backend set for twitter but authentication token fails.

--- server/functions.js
+++ server/functions.js
@@ -3,21 +3,21 @@ const config = require('./config');
 
 functions = {
   authorize: function(req, res) {
-    const header = config.consumerkey + ':' +config.consumersecret;
+    const header = config.consumerkey + ':' + config.consumersecret;
     const encheader = new Buffer(header).toString('base64');
     const finalheader = 'Basic ' + encheader;
 
-    request.post('https://api.twitter.com/oauth2/token', {form: {'grant_type': 'client_credentials'},
-      headers: {Authorization: finalheader}}, function(error, response, body) {
-      if(error)
-        console.log(error);
+    request.post('https://api.twitter.com/oauth2/token', { form: {'grant_type': 'client_credentials' },
+      headers: {Authorization: finalheader, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}}, function(error, response, body) {
+      if(error) throw error;
       else {
         console.log(body);
         config.bearertoken = JSON.parse(body).access_token;
-        res.json({success: true, data:config.bearertoken});
+        res.json({success: true, data: config.bearertoken});
       }
 
-    })
+    });
+
   }
 };
 
--- src/app/services/twitter.service.ts
+++ src/app/services/twitter.service.ts
@@ -7,9 +7,9 @@ export class TwitterService {
   constructor(protected http: HttpClient) { }
 
   makeCall() {
-    const headers = new HttpHeaders();
-    headers.append('Content-Type', 'application/X-www-form-urlencoded;charset=UTF-8');
-    this.http.post('http://localhost:3000/authorize', {headers: headers}).subscribe((res) => {
+    // const headers = new HttpHeaders();
+    // headers.append('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
+    this.http.post('http://localhost:3000/authorize', null).subscribe((res) => {
       console.log(res);
     });
 
