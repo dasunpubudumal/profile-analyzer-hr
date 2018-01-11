07d0603282c843113b2ca9c2c035f30b755f11b1
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2018-01-11 15:36:45 +0530

    stackoverflow api hit successful.

--- server/controllers/stackoverflow.js
+++ server/controllers/stackoverflow.js
@@ -1,14 +1,13 @@
 const request = require('request');
-const {stackOverflowConfig} = require('../config');
 
 stackOverflowFunctions = {
   getUser : (req, res) => {
     const URL = 'https://api.stackexchange.com/users/' + req.params.user_id + '?order=desc&sort=reputation&site=stackoverflow';
 
-    request.get(URL, {headers: {'Content-Type': 'application/json'}} ,(err, body, response) => {
+    request({method: 'GET', uri: URL, gzip: true},(err, body, response) => {
       if(err) throw err;
       else {
-        console.log(body);
+          console.log(body);  // Have to decode gzip in body.body.
         // res.json({success: true, data: JSON.parse(body)});
       }
     });
